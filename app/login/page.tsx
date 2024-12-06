9dc51fb0d44b28a05adcc77e4150d78f7a955f50
Ye-Joon Jung <179795735+jyjjung@users.noreply.github.com>
2024-12-06 10:48:05 +1000

    added login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -81,7 +81,7 @@ export default function Login() {
           </Button>
           <Spacer y={1} />
           <div className="text-center text-sm">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <a href="/signup" className="text-blue-600 hover:underline">
               Sign up
             </a>
